976f798a9c9fcc4ee883120639786667a4301a5b
Arjun Narzary <arjunn@byteridge.com>
2025-04-26 13:54:48 +0530

    Made header section responsive

--- src/app/(marketing)/_components/NavBar.tsx
+++ src/app/(marketing)/_components/NavBar.tsx
@@ -1,31 +1,82 @@
 import BrandLogo from "@/components/BrandLogo"
+import { Separator } from "@/components/ui/separator"
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet"
 import { SignedIn, SignedOut, SignInButton } from "@clerk/nextjs"
+import { MenuIcon, XIcon } from "lucide-react"
 import Link from "next/link"
 
 export default function NavBar() {
   return (
     <header className="flex py-6 shadow-xl fixed top-0 w-full z-10 bg-background/95">
-      <nav className="flex items-center gap-10 container font-semibold">
-        <Link href="/" className="mr-auto">
+      <nav className="flex justify-between items-center gap-10 container font-semibold">
+        <Link href="/">
           <BrandLogo />
         </Link>
-        <Link href="#" className="text-lg">
-          Features
-        </Link>
-        <Link href="/#pricing" className="text-lg">
-          Pricing
-        </Link>
-        <Link href="#" className="text-lg">
-          About
-        </Link>
-        <span className="text-lg">
-          <SignedIn>
-            <Link href="/dashboard">Dashboard</Link>
-          </SignedIn>
-          <SignedOut>
-            <SignInButton>Login</SignInButton>
-          </SignedOut>
-        </span>
+        <div className="hidden sm:flex justify-end items-center gap-10">
+          <Link href="#" className="text-lg">
+            Features
+          </Link>
+          <Link href="/#pricing" className="text-lg">
+            Pricing
+          </Link>
+          <Link href="#" className="text-lg">
+            About
+          </Link>
+          <span className="text-lg">
+            <SignedIn>
+              <Link href="/dashboard">Dashboard</Link>
+            </SignedIn>
+            <SignedOut>
+              <SignInButton>Login</SignInButton>
+            </SignedOut>
+          </span>
+        </div>
+        <div className="block sm:hidden">
+          <Sheet>
+            <SheetTrigger asChild>
+              <MenuIcon />
+            </SheetTrigger>
+            <SheetContent side={"left"} className="p-4">
+              <SheetHeader className="flex flex-row justify-between items-center w-full py-4">
+                <SheetTitle>
+                  <Link href="/">
+                    <BrandLogo />
+                  </Link>
+                </SheetTitle>
+                <SheetClose asChild>
+                  <XIcon />
+                </SheetClose>
+              </SheetHeader>
+              <Separator />
+              <div className="flex flex-col gap-4 py-4">
+                <Link href="#" className="text-lg">
+                  Features
+                </Link>
+                <Link href="/#pricing" className="text-lg">
+                  Pricing
+                </Link>
+                <Link href="#" className="text-lg">
+                  About
+                </Link>
+                <span className="text-lg">
+                  <SignedIn>
+                    <Link href="/dashboard">Dashboard</Link>
+                  </SignedIn>
+                  <SignedOut>
+                    <SignInButton>Login</SignInButton>
+                  </SignedOut>
+                </span>
+              </div>
+            </SheetContent>
+          </Sheet>
+        </div>
       </nav>
     </header>
   )
--- src/app/dashboard/_components/NavBar.tsx
+++ src/app/dashboard/_components/NavBar.tsx
@@ -1,18 +1,59 @@
 import BrandLogo from "@/components/BrandLogo"
+import { Separator } from "@/components/ui/separator"
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet"
 import { UserButton } from "@clerk/nextjs"
+import { MenuIcon, XIcon } from "lucide-react"
 import Link from "next/link"
 
 export default function NavBar() {
   return (
     <header className="flex py-4 shadow bg-background">
-      <nav className="flex items-center gap-10 container">
+      <nav className="flex justify-between items-center container">
         <Link className="mr-auto" href="/dashboard">
           <BrandLogo />
         </Link>
-        <Link href="/dashboard/products">Products</Link>
-        <Link href="/dashboard/analytics">Analytics</Link>
-        <Link href="/dashboard/subscription">Subscription</Link>
-        <UserButton />
+        <div className="flex justify-end gap-4 sm:gap-10">
+          <div className="hidden sm:flex justify-end gap-10">
+            <Link href="/dashboard/products">Products</Link>
+            <Link href="/dashboard/analytics">Analytics</Link>
+            <Link href="/dashboard/subscription">Subscription</Link>
+          </div>
+
+          <UserButton />
+
+          <div className="block sm:hidden">
+            <Sheet>
+              <SheetTrigger asChild>
+                <MenuIcon />
+              </SheetTrigger>
+              <SheetContent side={"left"} className="p-4">
+                <SheetHeader className="flex flex-row justify-between items-center w-full py-4">
+                  <SheetTitle>
+                    <Link href="/">
+                      <BrandLogo />
+                    </Link>
+                  </SheetTitle>
+                  <SheetClose asChild>
+                    <XIcon />
+                  </SheetClose>
+                </SheetHeader>
+                <Separator />
+                <div className="flex flex-col gap-4 py-4">
+                  <Link href="/dashboard/products">Products</Link>
+                  <Link href="/dashboard/analytics">Analytics</Link>
+                  <Link href="/dashboard/subscription">Subscription</Link>
+                </div>
+              </SheetContent>
+            </Sheet>
+          </div>
+        </div>
       </nav>
     </header>
   )
